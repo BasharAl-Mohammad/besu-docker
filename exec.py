32aed1ba9b859ea46adcb97a7baa292ef33c7c9f
BasharAl-Mohammad <bash.mh.99@gmail.com>
2023-08-29 16:06:52 +0200

    more refacturing

--- exec.py
+++
@@ -1,9 +0,0 @@
-import socket
-import subprocess
-
-
-hostname = socket.gethostname()
-# host_ip = socket.gethostbyname(socket.gethostname())
-host_ip = subprocess.run(['ip', 'route', 'get', '192.168.23.64'], stdout=subprocess.PIPE)
-host_ip = host_ip.stdout.decode().split(' ')
-host_ip = host_ip[host_ip.index('src') + 1]
